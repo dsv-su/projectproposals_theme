89e0ee6e68fecceba702fa7624e4cc7d8f8f3652
Pavel Sokolov <pavel.m.sokolov@gmail.com>
2018-04-13 11:17:09 +0300

    Fix approve-funding message due to wrong email address

--- js/edit.js
+++ js/edit.js
@@ -22,11 +22,11 @@ jQuery( function() {
     }
 
     if (approveby == 'funding-no') {
-      confirmmessage = "Do you want to report that your proposal have NOT been granted funding by the Funding Organization?\n\nIMPORTANT: Please also send the decision letter to register@dsv.su.se.";
+      confirmmessage = "Do you want to report that your proposal have NOT been granted funding by the Funding Organization?\n\nIMPORTANT: Please also send the decision letter to registrator@dsv.su.se.";
     }
 
     if (approveby == 'funding-yes') {
-      confirmmessage = "Do you want to report that your proposal have been granted funding by the Funding Organization?\n\nIMPORTANT: Please also send the decision letter to register@dsv.su.se";
+      confirmmessage = "Do you want to report that your proposal have been granted funding by the Funding Organization?\n\nIMPORTANT: Please also send the decision letter to registrator@dsv.su.se";
     }
 
     if (jQuery(button).hasClass('disabled')) {
